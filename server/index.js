4d177a211c23e9d9c622e40eec0242c05b6b76d1
suraj3641 <mauryasuraj364@gmail.com>
2024-06-14 17:10:19 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+import postRoutes from './routes/posts.js';
+
+const app =express();
+
+
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL='mongodb+srv://mauryasuraj364:surajmaurya12345@cluster0.keiyazf.mongodb.net/'
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
